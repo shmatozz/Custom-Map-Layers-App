ebc93e3156029348636eeefd41e643a64cc907d7
MaffyMaths <matvey33.baryshev@mail.ru>
2024-01-19 10:50:02 +0300

    basic app with map and button which get info from resource file

---
+++ app/src/main/java/com/example/custommaplayers/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.custommaplayers
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.maps.android.data.geojson.GeoJsonLayer
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mapView: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        val drawFromFileButton = findViewById<Button>(R.id.draw_from_file_btn)
+        val getFromServerButton = findViewById<Button>(R.id.get_from_server_btn)
+
+        drawFromFileButton.setOnClickListener {
+            getFromGeoJSON()
+        }
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        mapView = map
+    }
+
+    private fun getFromGeoJSON() {
+        val geoJSON = getJSONFromRawResource(applicationContext, R.raw.hse_cords)
+
+        // put info from JSON
+        val layer = GeoJsonLayer(mapView, geoJSON)
+        layer.addLayerToMap()
+
+        // zoom map
+        if (geoJSON.has("properties")) {
+            val properties = geoJSON.getJSONObject("properties")
+            if (properties.has("bbox")) {
+                val bboxArray = properties.getJSONArray("bbox")
+
+                val west = bboxArray.getDouble(0)
+                val south = bboxArray.getDouble(1)
+                val east = bboxArray.getDouble(2)
+                val north = bboxArray.getDouble(3)
+
+                val southwest = LatLng(south, west)
+                val northeast = LatLng(north, east)
+                val bounds = LatLngBounds(southwest, northeast)
+
+                mapView.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100))
+            }
+        }
+    }
+
+    private fun getJSONFromRawResource(context: Context, resourceId: Int): JSONObject {
+        val inputStream: InputStream = context.resources.openRawResource(resourceId)
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val stringBuilder = StringBuilder()
+
+        try {
+            var line = reader.readLine()
+            while (line != null) {
+                stringBuilder.append(line).append('\n')
+                line = reader.readLine()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            reader.close()
+        }
+
+        return JSONObject(stringBuilder.toString())
+    }
+}
