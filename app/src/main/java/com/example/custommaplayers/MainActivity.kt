2fa4986717cf1b57462d5340c5d2799d2e5e14af
MaffyMaths <matvey33.baryshev@mail.ru>
2024-01-24 10:13:14 +0300

    move data receive functions to special class

--- app/src/main/java/com/example/custommaplayers/MainActivity.kt
+++ app/src/main/java/com/example/custommaplayers/MainActivity.kt
@@ -1,29 +1,24 @@
 package com.example.custommaplayers
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.custommaplayers.data.DataProvider
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
 import com.google.maps.android.data.geojson.GeoJsonLayer
 import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.InputStream
-import java.io.InputStreamReader
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mapView: GoogleMap
     private lateinit var currentLayerDisplayed: GeoJsonLayer
+    private val dataProvider = DataProvider()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +31,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val getFromServerButton = findViewById<Button>(R.id.get_from_server_btn)
 
         drawFromFileButton.setOnClickListener {
-            getFromGeoJSON()
+            getFromJSONFile()
         }
 
         getFromServerButton.setOnClickListener {
@@ -48,52 +43,16 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mapView = map
     }
 
-    private fun getFromGeoJSON() {
-        val geoJSON = getJSONFromRawResource(applicationContext, R.raw.hse_cords)
+    private fun getFromJSONFile() {
+        val geoJSON = dataProvider.getJSONFromRawResource(applicationContext, R.raw.hse_cords)
 
-        // put info from JSON
         putLayerOnMap(geoJSON)
     }
 
-    private fun getJSONFromRawResource(context: Context, resourceId: Int): JSONObject {
-        val inputStream: InputStream = context.resources.openRawResource(resourceId)
-        val reader = BufferedReader(InputStreamReader(inputStream))
-        val stringBuilder = StringBuilder()
-
-        try {
-            var line = reader.readLine()
-            while (line != null) {
-                stringBuilder.append(line).append('\n')
-                line = reader.readLine()
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            reader.close()
-        }
-
-        return JSONObject(stringBuilder.toString())
-    }
-
     private fun getFromServer() {
-        val db = Firebase.firestore
-        val docRef = db.collection("geofiles").document("B1HY2BXZkYxrgu51quOa")
-        docRef.get()
-            .addOnSuccessListener { document ->
-                if (document != null) {
-                    Log.d("custom", "DocumentSnapshot data: ${document.data}")
-                    if (document.data != null) {
-                        putLayerOnMap(JSONObject(document.data!!))
-                    } else {
-                        Log.d("custom", "Document data is null")
-                    }
-                } else {
-                    Log.d("custom", "No such document")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.d("custom", "get failed with ", exception)
-            }
+        val geoJSONObject = dataProvider.getFromServer()
+
+        putLayerOnMap(geoJSONObject)
     }
 
     private fun putLayerOnMap(geoJSONObject: JSONObject) {
@@ -113,9 +72,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 val east = bboxArray.getDouble(2)
                 val north = bboxArray.getDouble(3)
 
-                val southwest = LatLng(south, west)
-                val northeast = LatLng(north, east)
-                val bounds = LatLngBounds(southwest, northeast)
+                val bounds = LatLngBounds(LatLng(south, west), LatLng(north, east))
 
                 mapView.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100))
             }
---
+++ app/src/main/java/com/example/custommaplayers/data/DataProvider.kt
@@ -0,0 +1,57 @@
+package com.example.custommaplayers.data
+
+import android.content.Context
+import android.util.Log
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+
+class DataProvider {
+    fun getFromServer() : JSONObject {
+        val db = Firebase.firestore
+        val docRef = db.collection("geofiles").document("B1HY2BXZkYxrgu51quOa")
+        var geoJSONObject = JSONObject()
+
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    if (document.data != null) {
+                        Log.d("custom", "DocumentSnapshot data: ${document.data}")
+                        geoJSONObject = JSONObject(document.data!!)
+                    } else {
+                        Log.d("custom", "Document data is null")
+                    }
+                } else {
+                    Log.d("custom", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("custom", "get failed with ", exception)
+            }
+
+        return geoJSONObject
+    }
+
+    fun getJSONFromRawResource(context: Context, resourceId: Int): JSONObject {
+        val inputStream: InputStream = context.resources.openRawResource(resourceId)
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val stringBuilder = StringBuilder()
+
+        try {
+            var line = reader.readLine()
+            while (line != null) {
+                stringBuilder.append(line).append('\n')
+                line = reader.readLine()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            reader.close()
+        }
+
+        return JSONObject(stringBuilder.toString())
+    }
+}
