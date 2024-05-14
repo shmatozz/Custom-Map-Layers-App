b5d1166d2246a216a2ffaa3d9d20601e76f16dfc
MaffyMaths <matvey33.baryshev@mail.ru>
2024-05-14 13:05:35 +0300

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,9 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-analytics")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
